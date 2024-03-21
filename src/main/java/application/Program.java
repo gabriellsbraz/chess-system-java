4058e39a64a59ce8c60b26bcb2f0bde42c4dec44
gabriellsbraz <gabriel_lsb@hotmail.com>
2024-03-21 01:19:56 -0300

    class Position

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        Position pos = new Position(4, 6);
+        System.out.println(pos);
     }
 }
